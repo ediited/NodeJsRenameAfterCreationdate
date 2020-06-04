2b13eb22f89f175949ccee3fcfed427a70d750da
Marie Steinbrügge <lauritz.kramberger@gmail.com>
2020-06-04 17:46:18 +0200

    Initial Commit, renaming working

---
+++ rename.js
@@ -0,0 +1,46 @@
+
+var path = require('path'), fs=require('fs');
+var fileName;
+var stats;
+var fromPath;
+var fileFormat;
+
+
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+
+});
+// if(process.argv){
+
+
+// }
+
+
+function getFilesFromPath(path, extension) {
+    let files = fs.readdirSync( path );
+    return files.filter( file => file.match(new RegExp(`.*\.(${extension})`, 'ig')));
+}
+var allVideos = getFilesFromPath(fromPath, fileFormat)
+
+
+console.log(allVideos);
+for(var i = 0; i<allVideos.length; i++){
+    stats = fs.statSync("./"+allVideos[i])
+    fileName = stats.birthtime.getFullYear()+"."+(stats.birthtime.getMonth()+1)+"."+(stats.birthtime.getDay()+1)
+    function renameFile(rfileName,copies){
+        if(fs.existsSync("./"+rfileName+"["+copies+"].mp4")){
+            renameFile(rfileName,parseInt(copies, 10) + 1
+            );
+        }
+        else{ 
+        fs.renameSync("./"+allVideos[i], "./"+rfileName+"["+copies+"]"+".mp4", function(err) {
+            if ( err ) console.log('ERROR: ' + err);
+            return;
+        });
+        console.log("Die Datei: "+allVideos[i]+" wird umbenannt in: "+stats.birthtime.getFullYear()+"."+(stats.birthtime.getMonth()+1)+"."+(stats.birthtime.getDay()+1)+"["+copies+"]");
+    }      
+        return;
+    }
+   renameFile(fileName,"0");
+}
+console.log("Done");
