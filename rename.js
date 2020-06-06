8b88f89cce0621bfb2e584545b021256d49883d3
Lauritz Krambeger <lauritz.kramberger@gmail.com>
2020-06-06 13:11:14 +0200

    added flag check and default values

--- rename.js
+++ rename.js
@@ -8,28 +8,28 @@ var dataFormat = "YYYYMMDD";
 
 var argv = require('minimist')(process.argv.slice(2));
 
-if(typeof argv.f != "undefined"){
-    if(argv.f.length>0){
+if (typeof argv.f != "undefined") {
+    if (argv.f.length > 0) {
         fileFormat = argv.f;
     }
 }
 
-if(typeof argv.p != "undefined"){
-    if(argv.p.length>0){
-     fromPath = argv.p; 
+if (typeof argv.p != "undefined") {
+    if (argv.p.length > 0) {
+        fromPath = argv.p;
     }
 }
 
-if(typeof argv.s != "undefined"){
-    switch(argv.s){
+if (typeof argv.s != "undefined") {
+    switch (argv.s) {
         case "DDMMYYYY":
-            ;
+            dataFormat = argv.s;
         case "MMDDYYYY":
-            ;
+            dataFormat = argv.s;
         case "YYYYMMDD":
-            ;
+            dataFormat = argv.s;
         case "YYYYDDMM":
-            ;
+            dataFormat = argv.s;
         default:
             Console.log("Format not recognised, using YYYYMMDD instead")
     }
@@ -46,7 +46,22 @@ var allVideos = getFilesFromPath(fromPath, fileFormat)
 console.log(allVideos);
 for (var i = 0; i < allVideos.length; i++) {
     stats = fs.statSync("./" + allVideos[i])
-    fileName = stats.birthtime.getFullYear() + "." + (stats.birthtime.getMonth() + 1) + "." + (stats.birthtime.getDay() + 1)
+    switch (dataFormat) {
+        case "DDMMYYYY":
+            fileName = (stats.birthtime.getDay() + 1) + "." + (stats.birthtime.getMonth() + 1) + "." + stats.birthtime.getFullYear()
+        case "MMDDYYYY":
+            fileName = (stats.birthtime.getMonth() + 1) + "." + (stats.birthtime.getDay() + 1) + "." + stats.birthtime.getFullYear()
+        case "YYYYMMDD":
+            fileName = stats.birthtime.getFullYear() + "." + (stats.birthtime.getMonth() + 1) + "." + (stats.birthtime.getDay() + 1)
+        case "YYYYDDMM":
+            fileName = stats.birthtime.getFullYear() + "." + (stats.birthtime.getDay() + 1) + "." + (stats.birthtime.getMonth() + 1)
+        default:
+        //Console.log("Format not recognised, using YYYYMMDD instead")
+    }
+
+
+
+
     function renameFile(rfileName, copies) {
         if (fs.existsSync("./" + rfileName + "[" + copies + "].mp4")) {
             renameFile(rfileName, parseInt(copies, 10) + 1
